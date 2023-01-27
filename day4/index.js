ba3985c7aa3bbcdd70d1880980e1e1c81d8e5954
cyndii0502 <90389093+cyndii20@users.noreply.github.com>
2023-01-28 01:25:06 +0900

    commit-codeChallenge_day4_day5

--- day4/index.js
+++ day4/index.js
@@ -1,11 +1,28 @@
-const body = document.querySelector("body");
+import "./style.css";
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 
-window.addEventListener("resize", () => {
-  if (window.innerWidth < 500) {
-    body.className = "color-small";
-  } else if (window.innerWidth < 800) {
-    body.className = "color-middle";
-  } else {
-    body.className = "color-large";
-  }
-});
+const title = document.querySelector("h2");
+
+const superEventHandler = {
+  mouseEnter: () => {
+    title.innerText = "Mouse Entered!";
+    title.style.color = colors[0];
+  },
+  mouseLeave: () => {
+    title.innerText = "Mouse Gone!";
+    title.style.color = colors[2];
+  },
+  windowResize: () => {
+    title.innerText = "Window Resized!";
+    title.style.color = colors[4];
+  },
+  rightClick: () => {
+    title.innerText = "Mouse Right Clicked!";
+    title.style.color = colors[3];
+  },
+};
+
+title.addEventListener("mouseenter", superEventHandler.mouseEnter);
+title.addEventListener("mouseleave", superEventHandler.mouseLeave);
+window.addEventListener("resize", superEventHandler.windowResize);
+window.addEventListener("contextmenu", superEventHandler.rightClick);
